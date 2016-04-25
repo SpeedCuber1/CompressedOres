c49056ed679fdfc18469cb7c457ecf4d03d78313
SpeedCuber1 <IpodTouchGuru123@Gmail.com>
2016-04-25 18:53:46 -0400

    Added Coal tier as Fuel Source

--- src/main/java/com/tylerh/compressedores/Proxy/CommonProxy.java
+++ src/main/java/com/tylerh/compressedores/Proxy/CommonProxy.java
@@ -1,9 +1,11 @@
 package com.tylerh.compressedores.Proxy;
 
 import com.tylerh.compressedores.Init.ModBlocks;
+import com.tylerh.compressedores.Util.CompressedOresFuelHandler;
 import com.tylerh.compressedores.Util.EventListener;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 /**
  * Created by Tyler on 11/6/2015.
@@ -16,6 +18,7 @@ public abstract class CommonProxy
     }
     public void init()
     {
+        GameRegistry.registerFuelHandler(new CompressedOresFuelHandler());
     }
     public void postInit()
     {
