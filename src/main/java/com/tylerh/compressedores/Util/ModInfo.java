f2aa4c0ae80ccc396d463403cb0aa03ce0f0b10b
tyler <IpodTouchGuru123@Gmail.com>
2018-03-01 18:21:43 -0500

    Updated Forge

--- src/main/java/com/tylerh/compressedores/Util/ModInfo.java
+++ src/main/java/com/tylerh/compressedores/Util/ModInfo.java
@@ -11,7 +11,7 @@ public class ModInfo
     public static final String MOD_VERSION = "1.14";
     public static final String CLIENT_PROXY = "com.tylerh.compressedores.Proxy.ClientProxy";
     public static final String SERVER_PROXY = "com.tylerh.compressedores.Proxy.ServerProxy";
-    public static final String GUI_FACTORY = "Gui.CompressedOresGuiFactory";
+    public static final String GUI_FACTORY = "com.tylerh.compressedores.Gui.CompressedOresGuiFactory";
     public static final String GUI_CATEGORY = "blocks";
     public static final boolean GUI_DEFAULT = true;
 
