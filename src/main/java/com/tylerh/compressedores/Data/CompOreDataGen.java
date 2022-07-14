147bd96a6a4739de50167fa84fc20d547b72fc30
SpeedCuber1 <IpodTouchGuru123@Gmail.com>
2022-07-13 20:35:30 -0400

    Updated Forge

--- src/main/java/com/tylerh/compressedores/Data/CompOreDataGen.java
+++ src/main/java/com/tylerh/compressedores/Data/CompOreDataGen.java
@@ -5,9 +5,9 @@ import com.tylerh.compressedores.Data.Loot_Tables.LootTables;
 import com.tylerh.compressedores.Data.Recipes.CompOreRecipe;
 import com.tylerh.compressedores.Data.Tags.CompOreBlockTag;
 import net.minecraft.data.DataGenerator;
+import net.minecraftforge.data.event.GatherDataEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class CompOreDataGen
