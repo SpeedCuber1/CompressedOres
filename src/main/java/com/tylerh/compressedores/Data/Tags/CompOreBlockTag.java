f98ab73e1f26baf938be237a0a223b7f2fac8274
SpeedCuber1 <IpodTouchGuru123@Gmail.com>
2023-03-07 10:50:49 -0500

    Reformatted Data Generators

--- src/main/java/com/tylerh/compressedores/Data/Tags/CompOreBlockTag.java
+++ src/main/java/com/tylerh/compressedores/Data/Tags/CompOreBlockTag.java
@@ -1,5 +1,6 @@
 package com.tylerh.compressedores.Data.Tags;
 
+import com.tylerh.compressedores.Util.EnumLevel;
 import com.tylerh.compressedores.Util.ModInfo;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.BlockTagsProvider;
@@ -15,50 +16,50 @@ public class CompOreBlockTag extends BlockTagsProvider
     @Override
     public void addTags()
     {
-        for(int i = 0; i < 8; i++)
+        for(EnumLevel level : EnumLevel.values())
         {
-            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.andesiteBlocks[i]);
-            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.basaltBlocks[i]);
-            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.blackstoneBlocks[i]);
-            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.calciteBlocks[i]);
-            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.clayBlocks[i]);
-            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.coalBlocks[i]);
-            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.cobbleBlocks[i]);
-            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.deepslateCobbleBlocks[i]);
-            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.diamondBlocks[i]);
-            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.dioriteBlocks[i]);
-            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.dirtBlocks[i]);
-            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.emeraldBlocks[i]);
-            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.goldBlocks[i]);
-            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.graniteBlocks[i]);
-            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.gravelBlocks[i]);
-            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.ironBlocks[i]);
-            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.lapisBlocks[i]);
-            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.quartzBlocks[i]);
-            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.redstoneBlocks[i]);
-            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.sandBlocks[i]);
-            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.tuffBlocks[i]);
-            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.andesiteBlocks[i]);
-            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.basaltBlocks[i]);
-            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.blackstoneBlocks[i]);
-            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.calciteBlocks[i]);
-            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.clayBlocks[i]);
-            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.coalBlocks[i]);
-            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.cobbleBlocks[i]);
-            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.deepslateCobbleBlocks[i]);
-            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.diamondBlocks[i]);
-            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.dioriteBlocks[i]);
-            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.dirtBlocks[i]);
-            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.emeraldBlocks[i]);
-            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.goldBlocks[i]);
-            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.graniteBlocks[i]);
-            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.gravelBlocks[i]);
-            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.ironBlocks[i]);
-            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.lapisBlocks[i]);
-            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.quartzBlocks[i]);
-            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.redstoneBlocks[i]);
-            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.sandBlocks[i]);
-            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.tuffBlocks[i]);
+            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.andesiteBlocks[level.getMetadata()]);
+            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.basaltBlocks[level.getMetadata()]);
+            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.blackstoneBlocks[level.getMetadata()]);
+            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.calciteBlocks[level.getMetadata()]);
+            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.clayBlocks[level.getMetadata()]);
+            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.coalBlocks[level.getMetadata()]);
+            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.cobbleBlocks[level.getMetadata()]);
+            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.deepslateCobbleBlocks[level.getMetadata()]);
+            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.diamondBlocks[level.getMetadata()]);
+            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.dioriteBlocks[level.getMetadata()]);
+            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.dirtBlocks[level.getMetadata()]);
+            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.emeraldBlocks[level.getMetadata()]);
+            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.goldBlocks[level.getMetadata()]);
+            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.graniteBlocks[level.getMetadata()]);
+            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.gravelBlocks[level.getMetadata()]);
+            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.ironBlocks[level.getMetadata()]);
+            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.lapisBlocks[level.getMetadata()]);
+            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.quartzBlocks[level.getMetadata()]);
+            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.redstoneBlocks[level.getMetadata()]);
+            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.sandBlocks[level.getMetadata()]);
+            tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ModInfo.tuffBlocks[level.getMetadata()]);
+            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.andesiteBlocks[level.getMetadata()]);
+            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.basaltBlocks[level.getMetadata()]);
+            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.blackstoneBlocks[level.getMetadata()]);
+            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.calciteBlocks[level.getMetadata()]);
+            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.clayBlocks[level.getMetadata()]);
+            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.coalBlocks[level.getMetadata()]);
+            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.cobbleBlocks[level.getMetadata()]);
+            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.deepslateCobbleBlocks[level.getMetadata()]);
+            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.diamondBlocks[level.getMetadata()]);
+            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.dioriteBlocks[level.getMetadata()]);
+            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.dirtBlocks[level.getMetadata()]);
+            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.emeraldBlocks[level.getMetadata()]);
+            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.goldBlocks[level.getMetadata()]);
+            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.graniteBlocks[level.getMetadata()]);
+            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.gravelBlocks[level.getMetadata()]);
+            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.ironBlocks[level.getMetadata()]);
+            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.lapisBlocks[level.getMetadata()]);
+            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.quartzBlocks[level.getMetadata()]);
+            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.redstoneBlocks[level.getMetadata()]);
+            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.sandBlocks[level.getMetadata()]);
+            tag(BlockTags.NEEDS_IRON_TOOL).add(ModInfo.tuffBlocks[level.getMetadata()]);
         }
     }
 }
