c061cc73aef89f9d63ce06b8ec6eb5ab2261c615
SpeedCuber1 <ipodtouchguru123@gmail.com>
2023-03-07 18:18:01 -0500

    Reformatted Data Generators Added Blockstates to Data Generators

---
+++ src/main/java/com/tylerh/compressedores/Data/Blockstates/CompOreBlockstateProvider.java
@@ -0,0 +1,51 @@
+package com.tylerh.compressedores.Data.Blockstates;
+
+import com.tylerh.compressedores.Util.EnumLevel;
+import com.tylerh.compressedores.Util.ModInfo;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+public class CompOreBlockstateProvider extends BlockStateProvider
+{
+    public CompOreBlockstateProvider(DataGenerator gen, ExistingFileHelper exFileHelper)
+    {
+        super(gen, ModInfo.MOD_ID, exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels()
+    {
+        for(EnumLevel level : EnumLevel.values())
+        {
+            blockWithItem(ModInfo.andesiteBlocks[level.getMetadata()]);
+            blockWithItem(ModInfo.basaltBlocks[level.getMetadata()]);
+            blockWithItem(ModInfo.blackstoneBlocks[level.getMetadata()]);
+            blockWithItem(ModInfo.calciteBlocks[level.getMetadata()]);
+            blockWithItem(ModInfo.clayBlocks[level.getMetadata()]);
+            blockWithItem(ModInfo.coalBlocks[level.getMetadata()]);
+            blockWithItem(ModInfo.cobbleBlocks[level.getMetadata()]);
+            blockWithItem(ModInfo.deepslateCobbleBlocks[level.getMetadata()]);
+            blockWithItem(ModInfo.diamondBlocks[level.getMetadata()]);
+            blockWithItem(ModInfo.dioriteBlocks[level.getMetadata()]);
+            blockWithItem(ModInfo.dirtBlocks[level.getMetadata()]);
+            blockWithItem(ModInfo.emeraldBlocks[level.getMetadata()]);
+            blockWithItem(ModInfo.goldBlocks[level.getMetadata()]);
+            blockWithItem(ModInfo.graniteBlocks[level.getMetadata()]);
+            blockWithItem(ModInfo.gravelBlocks[level.getMetadata()]);
+            blockWithItem(ModInfo.ironBlocks[level.getMetadata()]);
+            blockWithItem(ModInfo.lapisBlocks[level.getMetadata()]);
+            blockWithItem(ModInfo.netherrackBlocks[level.getMetadata()]);
+            blockWithItem(ModInfo.quartzBlocks[level.getMetadata()]);
+            blockWithItem(ModInfo.redstoneBlocks[level.getMetadata()]);
+            blockWithItem(ModInfo.sandBlocks[level.getMetadata()]);
+            blockWithItem(ModInfo.tuffBlocks[level.getMetadata()]);
+        }
+    }
+    private void blockWithItem(Block block)
+    {
+        simpleBlock(block);
+        simpleBlockItem(block,cubeAll(block));
+    }
+}
--- src/main/java/com/tylerh/compressedores/Data/CompOreDataGen.java
+++ src/main/java/com/tylerh/compressedores/Data/CompOreDataGen.java
@@ -1,6 +1,7 @@
 package com.tylerh.compressedores.Data;
 
 import com.tylerh.compressedores.Data.Advancements.BaseAdvancementProvider;
+import com.tylerh.compressedores.Data.Blockstates.CompOreBlockstateProvider;
 import com.tylerh.compressedores.Data.Loot_Tables.LootTables;
 import com.tylerh.compressedores.Data.Recipes.CompOreRecipe;
 import com.tylerh.compressedores.Data.Tags.CompOreBlockTag;
@@ -15,9 +16,11 @@ public class CompOreDataGen
     public static void gatherData(GatherDataEvent event)
     {
         var generator = event.getGenerator();
+        var existingFileHelper = event.getExistingFileHelper();
         generator.addProvider(true,new LootTables(generator));
         generator.addProvider(true,new BaseAdvancementProvider(generator));
         generator.addProvider(true,new CompOreRecipe(generator));
-        generator.addProvider(true,new CompOreBlockTag(generator,event.getExistingFileHelper()));
+        generator.addProvider(true,new CompOreBlockTag(generator,existingFileHelper));
+        generator.addProvider(true,new CompOreBlockstateProvider(generator,existingFileHelper));
     }
 }
